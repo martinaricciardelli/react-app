a3b90e1bb4580fb17d6b604b7ede64c632f9d380
martinaricciardelli <martina.ricciardelli@gmail.com>
2021-09-22 09:51:36 -0300

    se agregó img de productos

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -6,10 +6,11 @@ const Item = (producto) => {
     return (
         <div>
              <div class="card mt-5 ms-5" style={{width: "18rem"}}>
-                <img src="https://recetastips.com/wp-content/uploads/2020/05/helado-de-chocolate-6-1.jpg" class="card-img-top" alt="..."/>
+                <img src={producto.img} class="card-img-top" alt="..."/>
                  <div class="card-body">
                     <h5 class="card-title">{producto.titulo}</h5>
                     <p class="card-text">{producto.precio}</p> 
+                    <p class="card-text">{producto.stock}</p>
                 </div>
             <ItemCount stock={10} initial={1} onAdd={()=>{}}/>
         </div>
